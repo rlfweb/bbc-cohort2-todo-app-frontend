2b552a4404b45292c764d580732a2c3919ee3c16
James Heggs <james.heggs@techreturners.com>
2020-01-09 20:20:12 +0000

    Introduced state and props to app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import AddTask from './components/AddTask'
-import TaskCount from './components/TaskCount';
-import TaskList from './components/TaskList';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import AddTask from "./components/AddTask";
+import TaskCount from "./components/TaskCount";
+import TaskList from "./components/TaskList";
 
-function App() {
-  return (
-    <div className="container">
-      <Header />
-      <AddTask />
-      <TaskCount />
-      <TaskList />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    tasks: [
+      { description: "Buy some milk", completed: false },
+      { description: "Eat cheese", completed: true },
+      { description: "Walk the dog", completed: false },
+      { description: "Drink water", completed: false }
+    ]
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <AddTask />
+        <TaskCount />
+        <TaskList jobs={this.state.tasks} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Task from './Task';
+import Task from "./Task";
 
 class TaskList extends React.Component {
   render() {
     return (
-        <div>
-            <Task taskDescription="Buy some milk" />
-            <Task taskDescription="Pick-up dog food" />
-            <Task taskDescription="Buy some flowers" />
-        </div>
+      <div>
+        {this.props.jobs.map(function(item) {
+          return <Task taskDescription={item.description} />;
+        })}
+      </div>
     );
   }
 }
